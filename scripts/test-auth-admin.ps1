b754e6647162cf12cff3415a2d5fa087c543174b
morais705412 <morais705412@gmail.com>
2026-05-15 16:29:35 -0300

    chore: add official auth-admin smoke test script

---
+++ scripts/test-auth-admin.ps1
@@ -0,0 +1,21 @@
+$body = @'
+{
+  "action": "create-user",
+  "email": "teste.script@pontowebdesk.local",
+  "password": "123456",
+  "metadata": {
+    "nome": "Teste Script",
+    "cpf": "90000000002",
+    "company_id": "a145b0cd-76f4-4dc8-b50c-02b0c9bfe24b"
+  }
+}
+'@
+
+$body | Set-Content -Encoding utf8 body.json
+
+Invoke-WebRequest `
+  -Method POST `
+  -Uri "https://pontowebdesk.vercel.app/api/auth-admin" `
+  -ContentType "application/json" `
+  -InFile "body.json" |
+  Select-Object -ExpandProperty Content
